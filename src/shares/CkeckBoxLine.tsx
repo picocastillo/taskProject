1f2dd368177ffaf4bb0eb0176e97387a63f29a3d
Pico <castillo.cesar.pico@gmail.com>
2021-07-01 19:19:13 -0300

    Changed checkbox

--- src/shares/CkeckBoxLine.tsx
+++ src/shares/CkeckBoxLine.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import {CheckBox} from 'react-native';
+// import CheckBox from '@react-native-community/checkbox';
+import { CheckBox } from 'react-native-elements'
 import { Row,TextCheckbox } from '../utilStyles';
 import { check_4,check_1,check_2,check_3 } from '../constants';
 
@@ -12,20 +13,29 @@ export type Props = {
 
 const CheckBoxLine: React.FC<Props> = ({title,isCheck,changeState,id}) => {
     const [toggleCheckBox, setToggleCheckBox] = useState(isCheck)
-    const Checkbox = (value: boolean) => {
+    const onChange = (value: boolean) => {
         setToggleCheckBox(value);
         changeState(id);
       }
     return (
             <Row>
-                <CheckBox
+                {/* <CheckBox
                     value={isCheck}
                     onValueChange={Checkbox}
                     tintColor="#cc25aa"
                     onCheckColor="#12hdvc2"
                     boxType="circle"
+                    tintColors={{ true: (id%4)+1==1 ? check_1 : ((id%4)+1==2 ? check_2: ((id%4)+1==3 ? check_3 : check_4)), false: (id%4)+1==1 ? check_1 : ((id%4)+1==2 ? check_2: ((id%4)+1==3 ? check_3 : check_4)) }}
                 />
-                <TextCheckbox>{title}</TextCheckbox>
+                <TextCheckbox>{title}</TextCheckbox> */}
+                <CheckBox
+                    center
+                    title={title}
+                    checkedIcon='dot-circle-o'
+                    uncheckedIcon='circle-o'
+                    checked={isCheck}
+                    onPress={onChange}
+                  />
             </Row>
     )
 }
